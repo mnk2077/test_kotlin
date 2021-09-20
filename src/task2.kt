987406fa10e5409390bbd852a826938d963b84ed
mnk2077 <79050089+mnk2077@users.noreply.github.com>
2021-09-20 13:57:40 +0700

    Delete task2.kt

--- src/task2.kt
+++
@@ -1,8 +0,0 @@
-//Task2 "Сортировка списка"
-fun main(){
-    println("Please enter the line:")
-    val inputString = readLine()?.split(" ")?.toList() ?: return
-    println("Your line:")
-    val sortList = inputString.sorted()
-    sortList.forEach{el -> println(el)}
-}
