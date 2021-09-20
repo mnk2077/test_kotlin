f2931ffb1dc800299bff40130b2e6c6aa23a38fe
mnk2077 <79050089+mnk2077@users.noreply.github.com>
2021-09-20 13:57:27 +0700

    Delete task1.kt

--- src/task1.kt
+++
@@ -1,7 +0,0 @@
-//Task1 "Вывод списка слов"
-fun main(){
-    println("Please enter the line:")
-    val inputString = readLine()?.split(" ")?.toList() ?: return
-    println("Your line:")
-    inputString.forEach{el -> println(el)}
-}
