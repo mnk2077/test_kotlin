7079cf8dfc66e9f9fe7a33e8dbe9cbfeac17619d
mnk2077 <79050089+mnk2077@users.noreply.github.com>
2021-09-20 13:57:59 +0700

    Delete src directory

--- src/task3.kt
+++
@@ -1,9 +0,0 @@
-//Task3 "Вывод только уникальных слов из списка"
-fun main(){
-    println("Please enter the line:")
-    val inputString = readLine()?.split(" ")?.toList() ?: return
-    println("Your line:")
-    val sortList = inputString.sorted()
-    val uniqueList = sortList.toSet()
-    uniqueList.forEach{el -> println(el)}
-}
