cc3be06ddf9e1f1ff33de92c3fd5ba1af4074880
mnk2077 <79050089+mnk2077@users.noreply.github.com>
2021-10-16 01:10:06 +0700

    Delete src directory

--- src/task4.kt
+++
@@ -1,7 +0,0 @@
-//Task4 "Вывод количества повторений слов в списке"
-fun main() {
-    val inputString = readLine()?.split(" ")?.toList() ?: return
-    val sortList = inputString.sorted()
-    val countList = sortList.groupingBy { it }.eachCount().toSortedMap()
-    countList.forEach(::println)
-}
--- src/task5.kt
+++
@@ -1,8 +0,0 @@
-//Task5 "Сортировка количества повторений в порядке убывания"
-fun main() {
-    val inputString = readLine()?.split(" ")?.toList() ?: return
-    val sortList = inputString.sorted()
-    val countList = sortList.groupingBy { it }.eachCount().toSortedMap()
-    val sortCounted = countList.toList().sortedByDescending { (_, value) -> value }.toMap()
-    sortCounted.forEach(::println)
-}
