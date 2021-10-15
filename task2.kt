cb38d60bb0b9feb4fa7307ef190499a2195d1bbd
mnk2077 <79050089+mnk2077@users.noreply.github.com>
2021-10-16 01:14:05 +0700

    Delete task2.kt

--- task2.kt
+++
@@ -1,6 +0,0 @@
-//Task2 "Сортировка списка"
-fun main() {
-    val inputString = readLine()?.split(" ")?.toList() ?: return
-    val sortList = inputString.sorted()
-    sortList.forEach(::println)
-}
