55de4b4f0af0ded53b5b71a724b8623f679a8027
mnk2077 <79050089+mnk2077@users.noreply.github.com>
2021-10-16 01:23:35 +0700

    Delete task3.kt

--- src/task3.kt
+++
@@ -1,7 +0,0 @@
-//Task3 "Вывод только уникальных слов из списка"
-fun main() {
-    val inputString = readLine()?.split(" ")?.toList() ?: return
-    val sortList = inputString.sorted()
-    val uniqueList = sortList.distinct()
-    uniqueList.forEach(::println)
-}
