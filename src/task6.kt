c13af43bb6cc6c61bde726da7a9612585a85b387
Alexander <mnkwork3@gmail.com>
2021-11-03 17:29:57 +0700

    reformat task6

--- src/task6.kt
+++ src/task6.kt
@@ -1,7 +1,7 @@
 //Task6 "Вывод строки со стандартного потока"
-fun main(args: Array<String>){
-    if (args.isEmpty()) readLine()?.split(" ")?.toList()?.sorted()?.groupingBy { it }?.
-    eachCount()?.toSortedMap()?.toList()?.sortedByDescending { (_, value) -> value }?.toMap()?.forEach(::println)
-    else args.toList().sorted().groupingBy { it }.eachCount().toSortedMap().toList().
-    sortedByDescending { (_, value) -> value }.toMap().forEach(::println)
+fun main(args: Array<String>) {
+    if (args.isEmpty()) readLine()?.split(" ")?.toList()?.sorted()?.groupingBy { it }?.eachCount()?.toSortedMap()
+        ?.toList()?.sortedByDescending { (_, value) -> value }?.toMap()?.forEach(::println)
+    else args.toList().sorted().groupingBy { it }.eachCount().toSortedMap().toList()
+        .sortedByDescending { (_, value) -> value }.toMap().forEach(::println)
 }
