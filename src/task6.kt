c5af8a261df44faff64316ea35b058753429c799
Alexander <mnkwork3@gmail.com>
2021-11-09 23:22:02 +0700

    add new function in task6

--- src/task6.kt
+++ src/task6.kt
@@ -1,7 +1,11 @@
 //Task6 "Вывод строки со стандартного потока"
 fun main(args: Array<String>) {
-    if (args.isEmpty()) readLine()?.split(" ")?.toList()?.sorted()?.groupingBy { it }?.eachCount()?.toSortedMap()
-        ?.toList()?.sortedByDescending { (_, value) -> value }?.toMap()?.forEach(::println)
-    else args.toList().sorted().groupingBy { it }.eachCount().toSortedMap().toList()
-        .sortedByDescending { (_, value) -> value }.toMap().forEach(::println)
+    val inputList: List<String>? = if (args.isEmpty()) readLine()?.split(" ")?.toList()
+    else args.toList()
+    printList(inputList)
+}
+
+fun printList(args: List<String>?) {
+    args?.sorted()?.groupingBy { it }?.eachCount()?.toSortedMap()?.toList()
+        ?.sortedByDescending { (_, value) -> value }?.toMap()?.forEach(::println)
 }
