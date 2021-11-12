fe6ff814ea93647ff4bb73802ea07440c4fada2c
Alexander <mnkwork3@gmail.com>
2021-11-13 01:08:40 +0700

    Fix Build

--- BUILD.sh
+++ BUILD.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-startBuild = "kotlinc ./src/task$i.kt -include-runtime -d ./task$i.jar"
-for ((i = 1; i <= 6; i++))
-do
-  $startBuild
-done
+kotlinc src/task1.kt -include-runtime -d task1.jar
+kotlinc src/task2.kt -include-runtime -d task2.jar
+kotlinc src/task3.kt -include-runtime -d task3.jar
+kotlinc src/task4.kt -include-runtime -d task4.jar
+kotlinc src/task5.kt -include-runtime -d task5.jar
+kotlinc src/task6.kt -include-runtime -d task6.jar
