a6f70db77f01104c1294e528f86705cb31d04f4e
mainovmx <mainov_mx@live.ru>
2021-11-20 10:22:19 +0700

    add fun nowTime

--- src/project/Application.kt
+++ src/project/Application.kt
@@ -3,6 +3,8 @@ package project
 import project.userUtils.AuthHelper
 import project.userUtils.DataStorage
 import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) {
    Application()
@@ -16,7 +18,15 @@ class Application {
         onStart()
     }
 
+    private fun nowTime(){
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")
+        val formatted = current.format(formatter)
+        println("Текущее время:$formatted")
+    }
+
     private fun onStart() {
+        nowTime()
         dataStorage = DataStorage()
         doAuthUser()
     }
@@ -26,6 +36,7 @@ class Application {
             println("Пожалуйста, введите данные для входа")
             AuthHelper.getUserDataFromConsole().let {
                 val isUserAuthorized = AuthHelper.authUser(it, dataStorage)
+                nowTime()
                 if (isUserAuthorized) //TODO перекинуть в меню
                     else throw Exception("Error when user auth")
             }
