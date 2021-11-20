ec29d6f855040b38bb5c9d5c29baee38a3f0f27e
Alexander <mnkwork3@gmail.com>
2021-11-20 10:28:54 +0700

    delete comment

--- src/project/models/Resource.kt
+++ src/project/models/Resource.kt
@@ -1,11 +1,5 @@
 package project.models
 
-/*data class Resource(
-    val resourceA: String = "resource A",
-    val resourceB: String = "resource B",
-    val resourceC: String = "resource C",
-)*/
-
 open class ResourceA(){
     val resourceA: String = "resource A"
 }
